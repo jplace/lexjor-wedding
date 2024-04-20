d601d94ba49a10fc8f1ac98d964c6d930c733027
Jordan Place <jordan@replicahq.com>
2024-04-20 13:51:19 -0700

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,92 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const markdownIt = require("markdown-it");
+const fs = require("fs");
+const posthtml = require("posthtml");
+const beautifyHtml = require("js-beautify").html;
+const { DateTime } = require("luxon");
+
+module.exports = function (config) {
+  const isDev = process.env.PROJECT_DEV === "true";
+  if (isDev) {
+    console.log("Running with PROJECT_DEV set to true");
+  }
+
+  // Adding this just for the absoluteUrl filter used in 11ty examples
+  config.addPlugin(pluginRss);
+
+  // Support rendering data to markdown
+  let markdown = markdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  });
+  config.setLibrary("md", markdown);
+  config.addFilter("markdown", (value) => markdown.render(value));
+
+  // Formatting for dates
+  config.addFilter("readableDate", (dateStr) => {
+    return DateTime.fromISO(dateStr, { zone: "utc" }).toLocaleString(
+      DateTime.DATE_FULL
+    );
+  });
+  config.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  // Default href filter
+  config.addFilter("defaultHref", function (value, defaultHref) {
+    if (value === "#") {
+      return defaultHref;
+    } else {
+      return value;
+    }
+  });
+
+  // Pass through static assets
+  config.addPassthroughCopy("./src/site/images");
+  config.addPassthroughCopy("./src/site/fonts");
+  config.addPassthroughCopy("./src/site/_redirects");
+  config.addPassthroughCopy("./src/site/_headers");
+
+  // Optimize HTML
+  config.addTransform("posthtml", async function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      const { html } = await posthtml([
+        require("posthtml-alt-always")(),
+        require("posthtml-link-noreferrer")(),
+        require("htmlnano")({
+          minifySvg: false,
+        }),
+      ]).process(content);
+
+      if (isDev) {
+        return beautifyHtml(html, { indent_size: 2 });
+      } else {
+        return html;
+      }
+    }
+    return content;
+  });
+
+  // Browsersync to serve 404
+  config.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("./dist/404.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+  });
+
+  return {
+    dir: {
+      input: "src/site",
+      output: "dist",
+    },
+    templateFormats: ["njk", "11ty.js", "md"],
+  };
+};
