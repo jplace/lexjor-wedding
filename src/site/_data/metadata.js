001a557648a04afc811284f6df23fa84664a2eac
Jordan Place <tapman90@gmail.com>
2024-05-05 16:28:37 -0700

    fix redirect maybe

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "https://lexjorwedding.com", // use this only when absolute URLs are required
+  url: "https://lexjorwedding.netlify.app/", // use this only when absolute URLs are required
   defaultDescription: "Alexa & Jordan are getting married!",
 };
