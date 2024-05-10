1b96c385083a79180e200fe5b72c35f72cf5ecc9
Jordan Place <tapman90@gmail.com>
2024-05-09 20:51:16 -0700

    added link to help us get addresses

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        ['pulse-mild']: {
+          '50%': { color: '#033A15' } 
+        }
+      },
+      animation: {
+        ['pulse-mild']: 'pulse-mild 2s cubic-bezier(0.4, 0, 0.6, 1) infinite'
+      }
+    },
   },
   plugins: [],
 }
